6c2c7f8669c773a337b44a5d7fcda79d812ca7ef
ezequiel oggioni <ezequieloggioni@gmail.com>
2021-05-24 01:34:08 -0300

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd6f5d4e1fa535ca82ef736b1b2c1195582d8f7ce',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd6f5d4e1fa535ca82ef736b1b2c1195582d8f7ce',
+    ),
+  ),
+);
